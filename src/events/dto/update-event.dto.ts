4a036691e8e2bce7e30c812a88e547ccbf827b1c
khaled.njim <khaled.njim@esprit.tn>
2023-11-15 00:03:18 +0100

    final commit

--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,4 +1,18 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateEventDto } from './create-event.dto';
+import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto extends PartialType(CreateEventDto) {
+
+
+    @ApiProperty()
+    @IsNumber()
+    EventId: number;
+
+    @ApiProperty()
+    @IsNumber()
+    userId: number;
+
+
+
+}
--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, Put } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
@@ -23,11 +23,37 @@ export class EventsController {
     return this.eventsService.findAll();
   }
 
+  @Get("getEventsWithLikes/:idU")
+  findAllWithLiked( @Param('idU') idU: string) {
+    return this.eventsService.getEventsWithFavoriteAndParticipation(+idU);
+  }
+
+
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.eventsService.findOne(+id);
   }
 
+
+  @Patch("addEventParticipation")
+  addEventParticipation(@Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.addEventParticipation(updateEventDto);
+  }
+
+  @Patch("removeEventParticipation")
+  removeEventParticipation(@Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.removeEventParticipation(updateEventDto);
+  }
+
+  @Patch("removeEventfromFavorite")
+  removeEventfromFavorite(@Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.removeEventFromFavorite(updateEventDto);
+  }
+  @Patch("addEventToFavorite")
+  addEventToFavorite(@Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.addEventToFavorite(updateEventDto);
+  }
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
     return this.eventsService.update(+id, updateEventDto);
